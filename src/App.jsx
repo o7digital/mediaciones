666b9cc7a79df6f576c7f399e016d7a26d662d2e
o7digital <101292703+o7digital@users.noreply.github.com>
2025-07-21 21:30:21 -0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import HeroSection from './components/HeroSection';
+import AboutSection from './components/AboutSection';
+import ServicesSection from './components/ServicesSection';
+import CTASection from './components/CTASection';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <HeroSection />
+      <AboutSection />
+      <ServicesSection />
+      <CTASection />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
