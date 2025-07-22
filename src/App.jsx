c7d8077af2bc927cb10365866cd90cb4c4671099
o7digital <101292703+o7digital@users.noreply.github.com>
2025-07-22 07:50:37 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import HeroLawhere from './components/HeroLawhere';
 import ServicesShowcase from './components/ServicesShowcase';
+import WhyChooseUs from './components/WhyChooseUs';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <div className="pt-20">
         <HeroLawhere />
         <ServicesShowcase />
+        <WhyChooseUs />
       </div>
     </>
   );
