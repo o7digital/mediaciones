186858add0c670811ed7529c1d4d134a95b2db0a
o7digital <101292703+o7digital@users.noreply.github.com>
2025-07-26 07:55:06 -0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
+import './Footer.css';
 
 export default function Footer() {
   return (
-    <footer className="bg-black text-white py-6 text-center">
-      <p>
-        Correo: mediacionlegalprivada@gmail.com | Teléfono: 55 46 16 77 98
-      </p>
-      <p>
-        Dirección: Río Pánuco 43, Col. Renacimiento, Cuauhtémoc, CDMX
-      </p>
+    <footer className="footer">
+      <p>Correo: mediacionlegalprivada@gmail.com | Teléfono: 55 46 16 77 98</p>
+      <p>Dirección: Río Pánuco 43, Col. Renacimiento, Cuauhtémoc, CDMX.</p>
       <p>&copy; 2025 Mediaciones. Todos los derechos reservados.</p>
     </footer>
   );
