7e659eb416147db842ccc5c11ede754697687ee7
o7digital <101292703+o7digital@users.noreply.github.com>
2025-07-26 09:30:39 -0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,29 +5,31 @@ import { FaFacebookF, FaLinkedinIn, FaTiktok } from 'react-icons/fa';
 export default function Header() {
   return (
     <header className="header">
-      {/* Redes sociales */}
-      <div className="social-icons">
-        <a href="#" aria-label="Facebook"><FaFacebookF /></a>
-        <a href="#" aria-label="LinkedIn"><FaLinkedinIn /></a>
-        <a href="#" aria-label="TikTok"><FaTiktok /></a>
+      <div className="header-left">
+        {/* Redes sociales */}
+        <a href="#" className="social"><FaFacebookF /></a>
+        <a href="#" className="social"><FaLinkedinIn /></a>
+        <a href="#" className="social"><FaTiktok /></a>
+
+        {/* Logo / Nombre */}
+        <h1 className="logo">Mediaciones</h1>
       </div>
 
-      {/* Logo */}
-      <div className="logo">Mediaciones</div>
-
-      {/* Menú */}
+      {/* Menú de navegación */}
       <nav className="nav-menu">
         <a href="#">Home</a>
-        <a href="#">Quiénes somos</a>
-        <a href="#">Servicios</a>
-        <a href="#">Productos</a>
-        <a href="#">Contacto</a>
+        <a href="#quienes-somos">Quiénes somos</a>
+        <a href="#servicios">Servicios</a>
+        <a href="#productos">Productos</a>
+        <a href="#contacto">Contacto</a>
       </nav>
 
-      {/* Contacto */}
-      <div className="contact-info">
-        <span>+00-123-456789</span>
-        <span>info@mediaciones.com</span>
+      {/* Teléfono y correo */}
+      <div className="header-right">
+        <div className="contact-box">
+          <p className="phone">+00-123-456789</p>
+          <p className="email">info@mediaciones.com</p>
+        </div>
       </div>
     </header>
   );
