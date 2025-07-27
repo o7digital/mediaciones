bfc01e67a3b509f11b325d98078e49015bb2f6e2
o7digital <101292703+o7digital@users.noreply.github.com>
2025-07-26 22:34:13 -0600

    Update QuienesSomos.jsx

--- src/components/QuienesSomos.jsx
+++ src/components/QuienesSomos.jsx
@@ -4,7 +4,7 @@ export default function QuienesSomos() {
   return (
     <section className="quienes-somos-section" id="quienes-somos">
       <h2>¿Quiénes Somos?</h2>
-      <p className="intro-text">
+      <p>
         Somos un equipo de mediadores privados certificados por el Tribunal Superior de Justicia de la CDMX,
         especializados en ofrecer soluciones legales efectivas a través de la mediación legal privada. Nuestro
         objetivo es fortalecer las relaciones y lograr acuerdos sostenibles que reduzcan los tiempos y costos
